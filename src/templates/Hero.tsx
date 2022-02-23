ad2198891caa2964b21691d575933026ec693479
Hudson Chamberlain <hudson.m.chamberlain@gmail.com>
2022-02-23 10:50:09 -0600

    fix hero background

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -8,7 +8,7 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
-  <Background color='bg-black'>
+  <Background color='bg-transparent'>
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         {/* <li>
